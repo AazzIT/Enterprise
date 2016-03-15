1e39ab04e323ae21c394d97a45a224d2b9f90137
Anatolii <aazz@i.ua>
2016-03-16 01:58:01 +0200

    Class ArrayListCollection

--- src/collections/ArrayListCollection.java
+++ src/collections/ArrayListCollection.java
@@ -1,4 +1,76 @@
 package collections;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ListIterator;
+
 public class ArrayListCollection {
+    private final List<Integer> arrayList = new ArrayList<Integer>();
+
+    private long startTime;
+    private long estimatedTime;
+
+    public long add(int count) {
+        startTime = System.currentTimeMillis();
+        for (int i = 0; i < count; i++) {
+            this.arrayList.add(i);
+        }
+        estimatedTime = System.currentTimeMillis() - startTime;
+        return estimatedTime;
+    }
+
+    public long get(int count) {
+        startTime = System.currentTimeMillis();
+        for (int i = 0; i < count; i++) {
+            this.arrayList.get(i);
+        }
+        estimatedTime = System.currentTimeMillis() - startTime;
+        return estimatedTime;
+    }
+
+    public long remove(int count) {
+        startTime = System.currentTimeMillis();
+        for (int i = 0; i < count; i++) {
+            this.arrayList.remove(i);
+        }
+        estimatedTime = System.currentTimeMillis() - startTime;
+        return estimatedTime;
+    }
+
+    public long contains(int count) {
+        startTime = System.currentTimeMillis();
+        for (int i = 0; i < count; i++) {
+            this.arrayList.contains(i);
+        }
+        estimatedTime = System.currentTimeMillis() - startTime;
+        return estimatedTime;
+    }
+
+    public long populate() {
+        startTime = System.currentTimeMillis();
+        List<Integer> newArrayList  = new ArrayList<Integer>(arrayList);
+        estimatedTime = System.currentTimeMillis() - startTime;
+        return estimatedTime;
+    }
+
+    public long iteratorAdd() {
+        startTime = System.currentTimeMillis();
+        for (ListIterator<Integer> it = arrayList.listIterator(); it.hasNext();){
+            it.next();
+            it.add(it.previous());
+        }
+        estimatedTime = System.currentTimeMillis() - startTime;
+        return estimatedTime;
+    }
+
+    public long iteratorRemove() {
+        startTime = System.currentTimeMillis();
+        for (ListIterator<Integer> it = arrayList.listIterator(); it.hasNext();){
+            it.next();
+            it.remove();
+        }
+        estimatedTime = System.currentTimeMillis() - startTime;
+        return estimatedTime;
+    }
+
 }
--- src/collections/Main.java
+++ src/collections/Main.java
@@ -2,6 +2,16 @@ package collections;
 
 public class Main {
     public static void main(String[] args) {
-
+        long startTime = System.currentTimeMillis();
+        System.out.println(System.currentTimeMillis());
+        System.out.println(System.nanoTime());
+        System.out.println(System.currentTimeMillis());
+        System.out.println(System.nanoTime());
+        System.out.println(System.nanoTime());
+        System.out.println(System.nanoTime());
+        System.out.println(System.nanoTime());
+        System.out.println(System.nanoTime());
+        long estimatedTime = System.currentTimeMillis() - startTime;
+        System.out.println(estimatedTime);
     }
 }
